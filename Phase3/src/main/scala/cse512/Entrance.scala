47a832180822ec56bf6ca8d0de258c1009f31b74
Tarun Chinmai Sekar <tchinmai7@gmail.com>
2019-05-02 08:51:04 -0700

    Metrics collection code complete

--- Phase3/src/main/scala/cse512/Entrance.scala
+++ Phase3/src/main/scala/cse512/Entrance.scala
@@ -2,7 +2,6 @@ package cse512
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
-
 object Entrance extends App {
   Logger.getLogger("org.spark_project").setLevel(Level.WARN)
   Logger.getLogger("org.apache").setLevel(Level.WARN)
@@ -47,51 +46,55 @@ object Entrance extends App {
 
   private def queryLoader(spark: SparkSession, queryName: String, queryParams: String, outputPath: String) {
     val queryParam = queryParams.split(" ")
-    var queryResult:Long = -1
+    var dumpLocation = ""
     val stageMetrics = ch.cern.sparkmeasure.StageMetrics(spark)
+    //val stageMetrics = ch.cern.sparkmeasure.TaskMetrics(spark)
+    import spark.implicits._
     if (queryName.equalsIgnoreCase("hotcellanalysis")) {
       if (queryParam.length != 1) throw new ArrayIndexOutOfBoundsException("[CSE512] Query " + queryName + " needs 1 parameters but you entered " + queryParam.length)
-      stageMetrics.runAndMeasure(HotcellAnalysis.runHotcellAnalysis(spark, queryParam(0)).limit(50).write.mode(SaveMode.Overwrite).csv(outputPath))
       print("HotCell PerfMetrics")
-      val dump = spark.sql("select * from PerfStageMetrics").toDF()
-      dump.write.mode(SaveMode.Overwrite).json("test/perfHotCell")
-      dump.show()
+      stageMetrics.runAndMeasure(HotcellAnalysis.runHotcellAnalysis(spark, queryParam(0)).limit(50).write.mode(SaveMode.Overwrite).csv(outputPath))
+      dumpLocation = "test/perfHotCell"
     }
     else if (queryName.equalsIgnoreCase("hotzoneanalysis")) {
       if (queryParam.length != 2) throw new ArrayIndexOutOfBoundsException("[CSE512] Query " + queryName + " needs 2 parameters but you entered " + queryParam.length)
       print("HotZone PerfMetrics")
       stageMetrics.runAndMeasure(HotzoneAnalysis.runHotZoneAnalysis(spark, queryParam(0), queryParam(1)).write.mode(SaveMode.Overwrite).csv(outputPath))
-      val dump = spark.sql("select * from PerfStageMetrics").toDF()
-      dump.write.mode(SaveMode.Overwrite).json("test/perfHotZone")
-      dump.show()
-
+      dumpLocation = "test/perfHotZone"
     }
     else if (queryName.equalsIgnoreCase("RangeQuery"))
     {
       if(queryParam.length!=2) throw new ArrayIndexOutOfBoundsException("[CSE512] Query "+queryName+" needs 2 parameters but you entered "+queryParam.length)
-      queryResult = SpatialQuery.runRangeQuery(spark, queryParam(0), queryParam(1))
+      print("RangeQuery PerfMetrics")
+      stageMetrics.runAndMeasure(Seq(queryName, SpatialQuery.runRangeQuery(spark, queryParam(0), queryParam(1)).toString).toDF().write.mode(SaveMode.Overwrite).csv(outputPath))
+      dumpLocation = "test/perfRangeQuery"
     }
     else if (queryName.equalsIgnoreCase("RangeJoinQuery"))
     {
       if(queryParam.length!=2) throw new ArrayIndexOutOfBoundsException("[CSE512] Query "+queryName+" needs 2 parameters but you entered "+queryParam.length)
-      queryResult = SpatialQuery.runRangeJoinQuery(spark, queryParam(0), queryParam(1))
+      print("RangeJoinQuery PerfMetrics")
+      stageMetrics.runAndMeasure(Seq(queryName, SpatialQuery.runRangeJoinQuery(spark, queryParam(0), queryParam(1)).toString).toDF().write.mode(SaveMode.Overwrite).csv(outputPath))
+      dumpLocation = "test/perfRangeJoinQuery"
     }
     else if (queryName.equalsIgnoreCase("DistanceQuery"))
     {
       if(queryParam.length!=3) throw new ArrayIndexOutOfBoundsException("[CSE512] Query "+queryName+" needs 3 parameters but you entered "+queryParam.length)
-      queryResult = SpatialQuery.runDistanceQuery(spark, queryParam(0), queryParam(1), queryParam(2))
+      stageMetrics.runAndMeasure(Seq(queryName, SpatialQuery.runDistanceQuery(spark, queryParam(0), queryParam(1), queryParam(2)).toString).toDF().write.mode(SaveMode.Overwrite).csv(outputPath))
+      dumpLocation = "test/perfDistanceQuery"
     }
     else if (queryName.equalsIgnoreCase("DistanceJoinQuery"))
     {
       if(queryParam.length!=3) throw new ArrayIndexOutOfBoundsException("[CSE512] Query "+queryName+" needs 3 parameters but you entered "+queryParam.length)
-      queryResult = SpatialQuery.runDistanceJoinQuery(spark, queryParam(0), queryParam(1), queryParam(2))
+      stageMetrics.runAndMeasure(Seq(queryName, SpatialQuery.runDistanceJoinQuery(spark, queryParam(0), queryParam(1), queryParam(2)).toString).toDF().write.mode(SaveMode.Overwrite).csv(outputPath))
+      dumpLocation = "test/perfDistanceJoinQuery"
     }
     else {
       throw new NoSuchElementException("[CSE512] The given query name " + queryName + " is wrong. Please check your input.")
     }
+    val dump = spark.sql("select * from PerfStageMetrics").toDF()
 
-    import spark.implicits._
-    val resultDf = Seq(queryName, queryResult.toString).toDF()
-    resultDf.write.mode(SaveMode.Overwrite).csv(outputPath)
+    dump.show()
+    dump.coalesce(1).write.mode(SaveMode.Overwrite).option("header", "true").csv(dumpLocation)
+    spark.sql("drop table if exists PerfStageMetrics")
   }
 }
